c6934f3b150f30c2338b248ff4665786676a5ca0
Matin_MAB <matinmab789@gmail.com>
2022-04-06 18:46:10 +0430

    edit dashboboard-company to make its ui better

--- src/components/Dashboard/Company/DashboardCompany.vue
+++ src/components/Dashboard/Company/DashboardCompany.vue
@@ -21,20 +21,78 @@
         <v-col cols="0" lg="3" sm="2"></v-col>
       </v-row>
     </v-form>
+    <v-row class="mt-10 mx-16">
+      <v-col cols="12" lg="3" md="4" sm="6">
+        <v-card class="pa-4" outlined tile color="grey lighten-3" height="150">
+          <div class="d-flex justify-between align-center">
+            <v-card-title class="pa-0 text-h5 font-weight-bold"
+              >فایل</v-card-title
+            >
+            <v-spacer></v-spacer>
+
+            <v-icon> mdi-cloud-upload </v-icon>
+          </div>
+        </v-card>
+      </v-col>
+      <v-col cols="12" lg="3" md="4" sm="6">
+        <v-card class="pa-4" outlined tile color="grey lighten-3" height="150">
+          <div class="d-flex justify-between align-center">
+            <v-card-title class="pa-0 text-h5 font-weight-bold"
+              >فایل</v-card-title
+            >
+            <v-spacer></v-spacer>
+
+            <v-icon> mdi-cloud-upload </v-icon>
+          </div>
+        </v-card>
+      </v-col>
+      <v-col cols="12" lg="3" md="4" sm="6">
+        <v-card class="pa-4" outlined tile color="grey lighten-3" height="150">
+          <div class="d-flex justify-between align-center">
+            <v-card-title class="pa-0 text-h5 font-weight-bold"
+              >فایل</v-card-title
+            >
+            <v-spacer></v-spacer>
+
+            <v-icon> mdi-cloud-upload </v-icon>
+          </div>
+        </v-card>
+      </v-col>
+      <v-col cols="12" lg="3" md="4" sm="6">
+        <v-card class="pa-4" outlined tile color="grey lighten-3" height="150">
+          <div class="d-flex justify-between align-center">
+            <v-card-title class="pa-0 text-h5 font-weight-bold"
+              >فایل</v-card-title
+            >
+            <v-spacer></v-spacer>
+
+            <v-icon> mdi-cloud-upload </v-icon>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
     <div class="text-center">
-    <v-pagination
-      v-model="page"
-      :length="4"
-      prev-icon="mdi-menu-left"
-      next-icon="mdi-menu-right"
-    ></v-pagination>
-  </div>
+      <v-pagination class="mt-12 mb-16"
+        v-model="page"
+        :length="10"
+        circle
+        color="pallete1"
+        :total-visible="6"
+        prev-icon="mdi-menu-left"
+        next-icon="mdi-menu-right"
+      ></v-pagination>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "DashboardCompany",
+  data() {
+    return {
+      page: 1,
+    };
+  },
 };
 </script>
 
